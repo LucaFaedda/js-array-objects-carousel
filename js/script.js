2609dbdce2debb93052f4db7e69bbf2025096200
LucaFaedda <lucakarl91@gmail.com>
2022-12-14 20:37:43 +0100

    aggiunto array oggetti

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+const videogiochi = [
+    {
+        img: "01.webp",
+        titolo: "Spiderman: Miles Morales",
+        descrizione: "Il supereroe più amato di New York torna in una nuova veste"
+    },
+    {
+        img: "02.webp",
+        titolo: "Spiderman: Miles Morales",
+        descrizione: "Il supereroe più amato di New York torna in una nuova veste"
+    },
+    {
+        img: "03.webp",
+        titolo: "Spiderman: Miles Morales",
+        descrizione: "Il supereroe più amato di New York torna in una nuova veste"
+    },
+    {
+        img: "04.webp",
+        titolo: "Spiderman: Miles Morales",
+        descrizione: "Il supereroe più amato di New York torna in una nuova veste"
+    },
+    {
+        img: "05.webp",
+        titolo: "Spiderman: Miles Morales",
+        descrizione: "Il supereroe più amato di New York torna in una nuova veste"
+    }
+]
